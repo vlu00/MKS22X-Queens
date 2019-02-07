5576d19e59f6362894e763a074f6e56dee6b808b
vlu00 <vlu00@stuy.edu>
2019-02-07 17:31:43 -0500

    addQueen eliminates row and col queen is in but not diagonals yet

--- QueenBoard.java
+++ QueenBoard.java
@@ -11,14 +11,20 @@ public class QueenBoard{
   }
 
   private boolean addQueen(int row, int col) {
-    board[row][col] = -1;
-    int size = board.length;
-    for (int r = 0; r < size; r++) {
+    if (board[row][col] == 0) {
+      board[row][col] = -1;
+      int size = board.length;
       for (int c = 0; c < size; c++) {
-        if (r == row) {
-          board[r][c] =  board[r][c] + 1;
-        }
-        if 
+        board[row][c] = board[row][c] +1;
+      }
+      for (int r = 0; r < size; r++) {
+        board[r][col] = board[r][col] +1;
+      }
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   public String toString(){
@@ -35,6 +41,8 @@ public class QueenBoard{
 
   public static void main(String[] args) {
     QueenBoard A = new QueenBoard(4);
+    System.out.println(A.toString());
+    System.out.println(A.addQueen(0,0));
     System.out.println(A.toString());
   }
 
